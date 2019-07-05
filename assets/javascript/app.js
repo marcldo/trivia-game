5baa7e839f33da2d2d62fb58cdfe87121cdbe708
gitMarcldo <marcldo@users.noreply.github.com>
2019-07-05 00:24:40 -0400

    guess click events, results screen, checkAnswer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -29,11 +29,30 @@ const questions = [
 let time = 10;
 let intervalId;
 let questionNum = 0;
+let correct = null;
+let wrong = null;
 
 $(".start").on("click", function () {
     startGame();
     $(".start").css("display", "none");
     $(".timer").css("display", "block");
+});
+
+$(".a").on("click", function () {
+    //checkanswer
+    checkAnswer(questions[questionNum], "a");
+});
+$(".b").on("click", function () {
+    //checkanswer
+    checkAnswer(questions[questionNum], "b");
+});
+$(".c").on("click", function () {
+    //checkanswer
+    checkAnswer(questions[questionNum], "c");
+});
+$(".d").on("click", function () {
+    //checkanswer
+    checkAnswer(questions[questionNum], "d");
 });
 
 
@@ -46,11 +65,11 @@ function startGame() {
 function getNewQ() {
     if (questionNum < questions.length) {
         displayQnA(questions[questionNum]);
-        questionNum++;
+
         console.log(questionNum);
     }
     else {
-        startGame();
+        displayResults();
     }
 };
 
@@ -61,6 +80,37 @@ function displayQnA(obj) {
     $(".b").text(obj.b);
     $(".c").text(obj.c);
     $(".d").text(obj.d);
+};
+
+function displayResults() {
+    //Hide questions and choices
+    $(".question").css("display", "none");
+    $(".options").children().css("display", "none");
+    $(".timer").css("display", "none");
+
+    //set values
+    $(".correct").text("Correct answers: " + correct);
+    $(".wrong").text("wrong answers: " + wrong);
+
+    //display results
+    $(".results").children().css("display", "block");
+};
+
+function checkAnswer(obj, guess) {
+    console.log(obj[guess]);
+    if (obj[guess] === obj.answer) {
+        console.log("correct");
+        correct++;
+        //increment correct
+        //show right guess screen for 5 seconds
+    }
+    else {
+        console.log("wrong");
+        wrong++;
+        //increment wrong
+        //show wrong guess screen for 5 seconds
+    }
+    questionNum++;
 };
 
 //timer functions
@@ -88,4 +138,4 @@ function stopTimer() {
 function resetTimer() {
     time = 10;
     runTimer();
-}
+};
