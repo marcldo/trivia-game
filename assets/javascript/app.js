a80a33abb93184b8a0efd6048e8bfdd3b57352e1
gitMarcldo <marcldo@users.noreply.github.com>
2019-07-07 21:04:18 -0400

    sounds, 4 questions added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,27 +1,38 @@
 const questions = [
     {
-        question: "What color is the sky?",
-        a: "red",
-        b: "green",
-        c: "blue",
-        d: "yellow",
-        answer: "blue"
+        question: "AFTER A FRUSTRATING DAY OF SHOE SHOPPING AT THE MALL, NICK SWINMURN FOUNDED THIS WEBSITE",
+        a: "Ebay",
+        b: "Amazon",
+        c: "Adidas",
+        d: "Zappos",
+        answer: "Zappos",
+        category: "Websites"
     },
     {
-        question: "What color is the grass?",
-        a: "red",
-        b: "green",
-        c: "blue",
-        d: "yellow",
-        answer: "green"
+        question: "TAIPEI, IS THIS COUNTRY’S BIGGEST CITY",
+        a: "Vietnam",
+        b: "Cambodia",
+        c: "Taiwan",
+        d: "Japan",
+        answer: "Taiwan",
+        category: "Asian Geography"
     },
     {
-        question: "What color the sun?",
-        a: "red",
-        b: "green",
-        c: "blue",
-        d: "yellow",
-        answer: "yellow"
+        question: "LOCATED IN RHODE ISLAND, IT’S ALPHABETICALLY FIRST  AMONG IVY LEAGUE SCHOOLS",
+        a: "Columbia University",
+        b: "Brown University",
+        c: "Dartmouth College",
+        d: "Cornell University",
+        answer: "Brown University",
+        category: "Colleges & Universities"
+    },
+    {
+        question: "MICHELANGELO’S “PIETA” FEATURES THESE 2 FIGURES",
+        a: "Columbia University",
+        b: "Brown University",
+        c: "Dartmouth College",
+        d: "Cornell University",
+        answer: "Brown University"
     }
 
 ];
@@ -29,13 +40,27 @@ const questions = [
 let time = 10;
 let intervalId;
 let questionNum = 0;
-let correct = null;
-let wrong = null;
+let correct = 0;
+let wrong = 0;
+
+
+var theme = document.getElementById("theme");
+var audio = document.getElementById("audio");
+var timesUp = document.getElementById("timesUp");
+var correctDing = document.getElementById("correctDing");
+
+
+theme.play();
+
 
 $(".start").on("click", function () {
     startGame();
+
     $(".start").css("display", "none");
     $(".timer").css("display", "block");
+    $(".gameContainer").css("display", "block");
+    theme.src = "";
+    audio.src = "";
 });
 
 $(".a").on("click", function () {
@@ -55,34 +80,74 @@ $(".d").on("click", function () {
     checkAnswer(questions[questionNum], "d");
 });
 
-
+//starts game: gets first question and starts the timer
 function startGame() {
     questionNum = 0;
-    getNewQ();
     runTimer();
+    getNewQ();
+
 };
 
+
 function getNewQ() {
+    console.log(questionNum);
     if (questionNum < questions.length) {
         displayQnA(questions[questionNum]);
 
-        console.log(questionNum);
     }
     else {
-        displayResults();
+        setTimeout(function () { displayResults() }, 5000);
+        stopTimer();
+
     }
 };
 
 function displayQnA(obj) {
+
     console.log(obj);
     $(".question").text(obj.question);
     $(".a").text(obj.a);
     $(".b").text(obj.b);
     $(".c").text(obj.c);
     $(".d").text(obj.d);
+
+};
+
+function displayCorrectAnswer(obj, input) {
+
+    //Hide questions and choices
+    $(".question").css("display", "none");
+    $(".options").children().css("display", "none");
+    $(".timer").css("display", "none");
+
+    $(".status").text(input);
+    $(".answer").text(obj.answer);
+
+
+    //display answer 
+
+    $(".correctAnswer").children().css("display", "block");
+
+
+    setTimeout(function () {
+        console.log(questionNum);
+        $(".correctAnswer").children().css("display", "none");
+        if (questionNum < questions.length) {
+            $(".question").css("display", "block");
+            $(".options").children().css("display", "block");
+            $(".timer").css("display", "block");
+            $(".correctAnswer").children().css("display", "none");
+            resetTimer();
+        }
+    }, 5000);
+
+
+    //wait 5 seconds then display new question
+
 };
 
 function displayResults() {
+
     //Hide questions and choices
     $(".question").css("display", "none");
     $(".options").children().css("display", "none");
@@ -94,23 +159,38 @@ function displayResults() {
 
     //display results
     $(".results").children().css("display", "block");
+
 };
 
 function checkAnswer(obj, guess) {
+
     console.log(obj[guess]);
     if (obj[guess] === obj.answer) {
         console.log("correct");
         correct++;
+        displayCorrectAnswer(obj, "Correct!");
+        correctDing.play();
         //increment correct
         //show right guess screen for 5 seconds
     }
     else {
         console.log("wrong");
         wrong++;
+        displayCorrectAnswer(obj, "Wrong");
         //increment wrong
         //show wrong guess screen for 5 seconds
     }
-    questionNum++;
+
+    if (questionNum <= questions.length) {
+        questionNum++;
+        nextQuestion();
+    }
+};
+
+function nextQuestion() {
+    getNewQ();
+    stopTimer();
+    resetTimer();
 };
 
 //timer functions
@@ -124,10 +204,21 @@ function decrementTimer() {
     time--;
     $(".timer").text("Time left " + time);
 
+    if (time <= 5) {
+        $(".timer").css("color", "#E03A3E");
+    }
+
     if (time === 0) {
-        getNewQ();
         stopTimer();
-        resetTimer();
+
+        if (questionNum < questions.length) {
+            timesUp.play();
+            displayCorrectAnswer(questions[questionNum], "Time is up!");
+            questionNum++;
+            nextQuestion();
+
+        }
+
     }
 };
 
@@ -136,6 +227,8 @@ function stopTimer() {
 };
 
 function resetTimer() {
+    clearInterval(intervalId);
     time = 10;
     runTimer();
+    $(".timer").css("color", "#dddddd");
 };
